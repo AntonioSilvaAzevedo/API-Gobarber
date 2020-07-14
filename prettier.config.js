4461bbb6a5b314aec7c77532466506d0e0b72fa4
Antonio Carlos <antonie.dev@gmail.com>
2020-07-13 23:39:44 -0300

    list of providers

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,10 @@
+// export const singleQuote = true;
+// export const trailingComma = "all";
+// export const arrowParens = "avoid";
+
+
 module.exports = {
   singleQuote: true,
   trailingComma: "all",
-  arrowParens: "avoid",
+  arrowParens: "avoid"
 };
