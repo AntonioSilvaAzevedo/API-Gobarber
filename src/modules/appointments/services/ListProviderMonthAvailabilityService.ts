3a735a2a0bc16e35117cffeb9b17d559f1abb95c
Antonio Carlos <antonie.dev@gmail.com>
2020-07-20 15:19:25 -0300

    add mongoDB for notifications

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -39,7 +39,6 @@ class ListProviderMonthAvailabityService {
       { length: nunberOfDaysInMonth }, //tamanho do array
       (_, index) => index + 1,
     );
-    console.log(eachDayArray)
 
     const availability = eachDayArray.map(day => {
       const appointmentsInDay = appointments.filter( appointment => {
---
+++ src/modules/notifications/infra/typeorm/repositories/NotificationRepository.ts
@@ -0,0 +1,31 @@
+import {getMongoRepository, MongoRepository} from 'typeorm';
+
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO';
+
+import Notification from '../schemas/Notifications';
+
+class NotificationsRepository implements INotificationsRepository {
+  private ormRepository: MongoRepository<Notification>;
+
+  constructor() {
+    this.ormRepository = getMongoRepository(Notification, 'mongo');
+  }
+
+  public async create({
+    content,
+    recipient_id,
+  }: ICreateNotificationDTO): Promise<Notification> {
+    const notifications = this.ormRepository.create({
+      content,
+      recipient_id,
+    });
+
+    await this.ormRepository.save(notifications);
+
+    return notifications;
+  }
+
+}
+
+export default NotificationsRepository;
