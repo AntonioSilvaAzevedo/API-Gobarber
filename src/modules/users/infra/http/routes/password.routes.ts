9c1e1d86e6ddf8706ca2027cdbe4281b91bfbce6
Antonio Carlos <antonie.dev@gmail.com>
2020-07-22 11:51:50 -0300

    add Celebrate for routes validations

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -1,13 +1,35 @@
 import { Router } from 'express';
+import {celebrate, Segments, Joi } from 'celebrate';
+
 
 import ForgotPasswordController from '../controllers/ForgotpasswordController';
 import ResetPasswordController from '../controllers/ResetPasswordController';
+import { JoinColumn } from 'typeorm';
 
 const passwordRouter = Router();
 const forgotPaswordController = new ForgotPasswordController();
 const resetPasswordController = new ResetPasswordController();
 
-passwordRouter.post('/forgot', forgotPaswordController.create);
-passwordRouter.post('/reset', resetPasswordController.create);
+passwordRouter.post(
+  '/forgot',
+ celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().required(),
+    },
+  }),
+  forgotPaswordController.create
+);
+
+passwordRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+    resetPasswordController.create
+  );
 
 export default passwordRouter;
