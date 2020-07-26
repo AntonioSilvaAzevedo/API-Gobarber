01c74c8b8c05b42c4a58a2eff03ce64f0d11e2d2
Antonio Carlos <antonie.dev@gmail.com>
2020-07-26 14:46:10 -0300

    commit

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: process.env.APP_SECRET,
-    expiresIn: '1d',
+    secret: '66aad62f3b556181396042413692711f', // || process.env.APP_SECRET
+    expiresIn: '7d',
   },
 };
