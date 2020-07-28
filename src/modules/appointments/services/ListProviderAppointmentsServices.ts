9de8239b558fd9f90310a3fada1bafd479944638
Antonio Carlos <antonie.dev@gmail.com>
2020-07-28 17:04:16 -0300

    add cache fom providersAppointments

--- src/modules/appointments/services/ListProviderAppointmentsServices.ts
+++ src/modules/appointments/services/ListProviderAppointmentsServices.ts
@@ -27,20 +27,27 @@ class ListProviderAppointmentsService{
     month,
     day
   }: IRequest): Promise<Appointment[]> {
-    const cacheData = await this.cacheProvider.recover('singed');
+    const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
 
-    console.log(cacheData);
-
-    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
-      {
-        provider_id,
-        year,
-        month,
-        day,
-      },
+    let appointments = await this.cacheProvider.recover<Appointment[]>(
+      cacheKey,
     );
 
-    // await this.cacheProvider.save('jax', 'singed');
+    if (!appointments) {
+      appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+        {
+          provider_id,
+          year,
+          month,
+          day,
+        },
+      );
+
+      await this.cacheProvider.save(cacheKey, appointments);
+    }
+
+    console.log('Buscou do banco');
+
 
     return appointments;
 
