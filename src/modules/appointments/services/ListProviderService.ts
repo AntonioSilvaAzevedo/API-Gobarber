6a5b38a6afa82bfe95d57d2146338c4254ab2561
Antonio Carlos <antonie.dev@gmail.com>
2020-08-16 22:38:19 -0300

    comitão of sunday

--- src/modules/appointments/services/ListProviderService.ts
+++ src/modules/appointments/services/ListProviderService.ts
@@ -1,4 +1,5 @@
 import {inject, injectable} from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
@@ -24,12 +25,16 @@ class ListProvidersService {
       `providers-list:${user_id}`,
     );
 
+
+
     if(!users) {
       users = await this.usersRepository.findAllProviders({
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users));
     }
 
     return users;
